b734e17e472dd29fafc68117797a9baad822ceac
Sheelove Dol <siesel26@gmail.com>
2024-01-24 14:55:49 -0800

    Removed previous bluetooth package and added flutter-blue-plus

--- windows/flutter/generated_plugin_registrant.cc
+++ windows/flutter/generated_plugin_registrant.cc
@@ -6,9 +6,6 @@
 
 #include "generated_plugin_registrant.h"
 
-#include <bluetooth_low_energy_windows/bluetooth_low_energy_windows_c_api.h>
 
 void RegisterPlugins(flutter::PluginRegistry* registry) {
-  BluetoothLowEnergyWindowsCApiRegisterWithRegistrar(
-      registry->GetRegistrarForPlugin("BluetoothLowEnergyWindowsCApi"));
 }
