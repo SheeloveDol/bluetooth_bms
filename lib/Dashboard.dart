17cb31291d1c68ab0996ebef97f7dde135572bb2
sunny <sunny@sunnys-MacBook-Pro.local>
2024-03-03 22:51:06 -0500

    fixing charge discharge

--- lib/Dashboard.dart
+++ lib/Dashboard.dart
@@ -73,7 +73,7 @@ class _DashBoardState extends State<DashBoard> {
         Data.setAvailableData(true);
         setState(() {});
         _timer =
-            Timer.periodic(const Duration(milliseconds: 700), (timer) async {
+            Timer.periodic(const Duration(milliseconds: 1500), (timer) async {
           if (!Be.communicatingNow) {
             (alternate) ? await Be.getBasicInfo() : await Be.getCellInfo();
             alternate = !alternate;
