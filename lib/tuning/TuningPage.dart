f80f3da0d126c33420248a402e9c3754bbad1a87
sunny <sunny@sunnys-MacBook-Pro.local>
2024-07-09 04:57:16 -0400

    new update

--- lib/tuning/TuningPage.dart
+++ lib/tuning/TuningPage.dart
@@ -1,4 +1,5 @@
 import 'package:bluetooth_bms/settings/OneInputField.dart';
+import 'package:bluetooth_bms/src.dart';
 import 'package:bluetooth_bms/tuning/GroupButton.dart';
 import 'package:bluetooth_bms/tuning/TuningSection.dart';
 import 'package:flutter/material.dart';
@@ -25,6 +26,55 @@ class _TuningPageStae extends State<TuningPage> {
     super.dispose();
   }
 
+  OneInputField cellmV(String title, int index) {
+    Color color = Colors.black;
+    Color titleColor = Colors.black;
+    FontWeight weight = FontWeight.normal;
+    FontWeight titleWeight = FontWeight.normal;
+    if (Data.bal[index]) {
+      titleColor = Colors.blue;
+      titleWeight = FontWeight.bold;
+    }
+    if (Data.cell_mv[index] > Data.bal_start) {
+      color = Color(0xFFCA5100);
+      weight = FontWeight.bold;
+    }
+    if (Data.cell_mv[index] < Data.bal_start) {
+      color = Colors.yellow;
+      weight = FontWeight.bold;
+    }
+    return OneInputField(
+        text: title,
+        initialValue: "${Data.cell_mv[index].toStringAsFixed(3)}",
+        onChange: (v) {});
+  }
+
+  OneInputField cellmA(String title, int index) {
+    Color color = Colors.black;
+    Color titleColor = Colors.black;
+    FontWeight weight = FontWeight.normal;
+    FontWeight titleWeight = FontWeight.normal;
+    if (Data.bal[index]) {
+      titleColor = Colors.blue;
+      titleWeight = FontWeight.bold;
+    }
+    if (Data.cell_mv[index] > Data.bal_start) {
+      color = Color(0xFFCA5100);
+      weight = FontWeight.bold;
+    }
+    if (Data.cell_mv[index] < Data.bal_start) {
+      color = Colors.yellow;
+      weight = FontWeight.bold;
+    }
+    return OneInputField(
+        text: title, initialValue: "cell ma $index", onChange: (v) {});
+  }
+
+  OneInputField temp(String title, int index) {
+    return OneInputField(
+        text: title, initialValue: Data.ntc_temp[index], onChange: (v) {});
+  }
+
   @override
   Widget build(BuildContext context) {
     return SafeArea(
@@ -47,31 +97,31 @@ class _TuningPageStae extends State<TuningPage> {
                         TuningSection(
                             title: "Modify Cell Voltage",
                             settingsElements: [
-                              OneInputField(
-                                  text: "Number of Cells", onChange: (v) {}),
-                              OneInputField(
-                                  text: "Number of Cells", onChange: (v) {}),
-                              OneInputField(
-                                  text: "Number of Cells", onChange: (v) {}),
+                              for (int i = 0; i < Data.cell_cnt; i++)
+                                cellmV("cell${i + 1}", i)
                             ]),
                         TuningSection(
                             title: "Modify Cell Amperage",
-                            settingsElements: []),
+                            settingsElements: [
+                              for (int i = 0; i < Data.cell_cnt; i++)
+                                cellmA("cell${i + 1}", i)
+                            ]),
                         TuningSection(
                             title: "Modify Temperature Sensor",
-                            settingsElements: []),
+                            settingsElements: [
+                              for (int i = 0; i < Data.ntc_cnt; i++)
+                                cellmV("T${i + 1}", i)
+                            ])
                       ])),
-              StatefulBuilder(
-                builder: (BuildContext context, setThisState) {
-                  setSpecificState = () => setThisState(() {});
-                  return GroupButton(
-                      index: index,
-                      onChanged: (int i) {
-                        _controller.animateToPage(i,
-                            duration: Durations.long4, curve: Easing.standard);
-                      });
-                },
-              ),
+              StatefulBuilder(builder: (BuildContext context, setThisState) {
+                setSpecificState = () => setThisState(() {});
+                return GroupButton(
+                    index: index,
+                    onChanged: (int i) {
+                      _controller.animateToPage(i,
+                          duration: Durations.long4, curve: Easing.standard);
+                    });
+              })
             ])));
   }
 }
