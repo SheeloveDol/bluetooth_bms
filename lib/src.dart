471565d417f2edde8a62d38575fe143bea0318e7
sunny <derisme@live.ca>
2025-02-12 00:12:37 -0500

    betting badge final fix

--- lib/src.dart
+++ lib/src.dart
@@ -1050,7 +1050,7 @@ class Data {
 
   /// Change form  Legacy parameters
   static int fromLegacy(int pegacyParam) {
-    if (pegacyParam > legacy(ADV_LOW_V_TRIG) || pegacyParam < legacy(0)) {
+    if (pegacyParam > 0x37 || pegacyParam < 0x10) {
       throw "Unaceptable legacy parameter translation";
     }
     return pegacyParam - 0x10;
